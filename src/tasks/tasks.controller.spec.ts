90b2227983109cea5e6fe413afd63a628b8ee95d
Luciano Schmarsow <78238612+clarkos@users.noreply.github.com>
2023-08-10 00:56:12 +0100

    test: :white_check_mark: implement test suites
    added various test for the tasks controller and service

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
-import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
+import { TaskStatus } from './tasks.model';
 
 describe('TasksController', () => {
   let controller: TasksController;
@@ -9,12 +9,71 @@ describe('TasksController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TasksController],
+      providers: [TasksService],
     }).compile();
 
     controller = module.get<TasksController>(TasksController);
   });
 
-  xit('should be defined', () => {
+  it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  // Tests that getAllTasks() returns an array of tasks
+  it('should return an array of tasks', () => {
+    const result = controller.getAllTasks();
+    expect(Array.isArray(result)).toBe(true);
+  });
+
+  // Tests that getTaskById(id) returns the correct task
+  it('should return the correct task', () => {
+    const task = controller.createTask({
+      title: 'Test Task',
+      description: 'Test Description',
+    });
+    const result = controller.getTaskById(task.id);
+    expect(result).toEqual(task);
+  });
+
+  // Tests that deleteTask(id) removes the correct task
+  it('should remove the correct task', () => {
+    const task = controller.createTask({
+      title: 'Test Task',
+      description: 'Test Description',
+    });
+    controller.deleteTask(task.id);
+    const result = controller.getTaskById(task.id);
+    expect(result).toBeUndefined();
+  });
+
+  // Tests that updateTask(id, status) updates the correct task
+  it('should update the correct task', () => {
+    const task = controller.createTask({
+      title: 'Test Task',
+      description: 'Test Description',
+    });
+    const updatedTask = controller.updateTask(task.id, TaskStatus.IN_PROGRESS);
+    expect(updatedTask.status).toEqual(TaskStatus.IN_PROGRESS);
+  });
+
+  // Tests that createTask(createTaskDto) creates a new task
+  it('should create a new task', () => {
+    const createTaskDto = {
+      title: 'Test Task',
+      description: 'Test Description',
+    };
+    const result = controller.createTask(createTaskDto);
+    expect(result.title).toEqual(createTaskDto.title);
+    expect(result.description).toEqual(createTaskDto.description);
+  });
+
+  // Tests that createTask(createTaskDto) throws an error if title is empty or description is empty
+  it('should throw an error if title is empty or description is empty', () => {
+    expect(() =>
+      controller.createTask({ title: '', description: 'Test Description' }),
+    ).toThrowError('title is required');
+    expect(() =>
+      controller.createTask({ title: 'Test Task', description: '' }),
+    ).toThrowError('description is required');
   });
 });
--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './tasks.model';
 
 describe('TasksService', () => {
   let service: TasksService;
@@ -14,5 +16,89 @@ describe('TasksService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  // Tests that createTask successfully creates a task with valid title and description, and adds it to the tasks array
+  it('should create a task with valid title and description and add it to the tasks array', () => {
+    const createTaskDto: CreateTaskDto = {
+      title: 'Test Task',
+      description: 'This is a test task',
+    };
+    const task = service.createTask(createTaskDto);
+    expect(task).toBeDefined();
+    expect(task.id).toBeDefined();
+    expect(task.title).toEqual(createTaskDto.title);
+    expect(task.description).toEqual(createTaskDto.description);
+    expect(task.status).toEqual(TaskStatus.OPEN);
+    expect(service.getAllTasks()).toContain(task);
+  });
+
+  // Tests that createTask throws an error when description is empty
+  it('should throw an error when description is empty', () => {
+    const createTaskDto: CreateTaskDto = {
+      title: 'Test Task',
+      description: '',
+    };
+    expect(() => service.createTask(createTaskDto)).toThrow();
+  });
+
+  // Tests that createTask throws an error when title is empty
+  it('should throw an error when title is empty', () => {
+    const createTaskDto: CreateTaskDto = {
+      title: '',
+      description: 'This is a test task',
+    };
+    expect(() => service.createTask(createTaskDto)).toThrow();
+  });
+
+  // Tests that multiple tasks created have unique ids
+  it('should create multiple tasks with unique ids', () => {
+    const createTaskDto1: CreateTaskDto = {
+      title: 'Test Title 1',
+      description: 'Test Description 1',
+    };
+    const createTaskDto2: CreateTaskDto = {
+      title: 'Test Title 2',
+      description: 'Test Description 2',
+    };
+    const task1 = service.createTask(createTaskDto1);
+    const task2 = service.createTask(createTaskDto2);
+    expect(task1.id).toBeDefined();
+    expect(task2.id).toBeDefined();
+    expect(task1.id).not.toEqual(task2.id);
+  });
+
+  // Tests that a task can be created and retrieved by its id
+  it('should create a task and retrieve it by its id', () => {
+    const createTaskDto: CreateTaskDto = {
+      title: 'Test Title',
+      description: 'Test Description',
+    };
+    const task = service.createTask(createTaskDto);
+    const retrievedTask = service.getTaskById(task.id);
+    expect(retrievedTask).toEqual(task);
+  });
+
+  // Tests that an empty array is returned when there are no tasks
+  it('should return an empty array when there are no tasks', () => {
+    const tasks = service.getAllTasks();
+    expect(tasks).toEqual([]);
+  });
+
+  // Tests that a task can be created and deleted by its id
+  it('should create a task and delete it by its id', () => {
+    const createTaskDto: CreateTaskDto = {
+      title: 'Test Task',
+      description: 'This is a test task',
+    };
+    const task = service.createTask(createTaskDto);
+    expect(task).toBeDefined();
+    expect(task.id).toBeDefined();
+    expect(task.title).toEqual(createTaskDto.title);
+    expect(task.description).toEqual(createTaskDto.description);
+    expect(task.status).toEqual(TaskStatus.OPEN);
+    expect(service.getAllTasks()).toContain(task);
+    service.deleteTask(task.id);
+    expect(service.getTaskById(task.id)).toBeUndefined();
   });
 });
